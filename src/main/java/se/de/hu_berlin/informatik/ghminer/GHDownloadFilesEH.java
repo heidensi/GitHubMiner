9e4208f3385245b3684f077972e08609417684d4
heidensi <heiden@informatik.hu-berlin.de>
2016-10-23 22:26:47 +0200

    download refactored

--- src/main/java/se/de/hu_berlin/informatik/ghminer/GHDownloadFilesEH.java
+++ src/main/java/se/de/hu_berlin/informatik/ghminer/GHDownloadFilesEH.java
@@ -1,17 +1,11 @@
 package se.de.hu_berlin.informatik.ghminer;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import se.de.hu_berlin.informatik.utils.miscellaneous.Log;
+import se.de.hu_berlin.informatik.utils.fileoperations.FileUtils;
 import se.de.hu_berlin.informatik.utils.threaded.disruptor.eventhandler.EHWithInputAndReturn;
 import se.de.hu_berlin.informatik.utils.threaded.disruptor.eventhandler.EHWithInputAndReturnFactory;
 
 /**
- * A simple thread to download a file from git hub
+ * A simple event handler that downloads a file from git hub
  */
 public class GHDownloadFilesEH extends EHWithInputAndReturn<GHTreeEntryWrapper, Object> {
 
@@ -23,30 +17,7 @@ public class GHDownloadFilesEH extends EHWithInputAndReturn<GHTreeEntryWrapper, 
 	 */
 	@Override
 	public Object processInput(GHTreeEntryWrapper input) {
-		FileOutputStream fos = null;
-
-		try {
-			File tar_f = input.getOutputPath().toFile();
-
-			// Log.out(this, "Downloading " + ghte.getDownloadURL() + " to " + tar_f);
-			URL website = new URL(input.getDownloadURL());
-			// Log.out(this, "Thread: " + Thread.currentThread().getId());
-
-			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-			fos = new FileOutputStream(tar_f);
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-		} catch (IOException e) {
-			Log.err(this, e, "Error with content " + input.getDownloadURL());
-		} finally {
-			if (fos != null) {
-				try {
-					fos.close();
-				} catch (IOException e) {
-					// nothing to do
-				}
-			}
-		}
-		
+		FileUtils.downloadFile(input.getDownloadURL(), input.getOutputPath().toFile());
 		return null;
 	}
 	
